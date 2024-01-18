a36c80ed11aff53b56c4a7012e78c369fd3f67ae
Keith Suderman <suderman@jhu.edu>
2024-01-18 13:05:32 -0500

    Process multiple tools at once

--- rnaseq/bin/summarize-files.sh
+++ rnaseq/bin/summarize-files.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 
-TOOLS="rna_star cutadapt"
-tool=$1
+#TOOLS="rna_star cutadapt"
+TOOLS=$@
 
 for col in d e f g h ; do
 	for file in $(find results -name "*_costs_col-$col*.md") ; do
 		file_parts=$(echo $file | awk -F_ '{print $(NF-2),$(NF-1)}')
-		#for tool in $TOOLS ; do
-		echo "$file_parts $tool $(bin/average.py -t $tool $file)"
-		#done
+		for tool in $TOOLS ; do
+			echo "$file_parts $tool $(bin/average.py -t $tool $file)"
+		done
 	done
 done
